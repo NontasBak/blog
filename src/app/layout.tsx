ef73140ca4def4347a5956763afcd3fcdca0d9da
NontasBak <nontasbak@gmail.com>
2025-10-02 01:25:04 +0300

    Remove layout shift with next/font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,31 @@
 import "@/app/global.css";
 import { RootProvider } from "fumadocs-ui/provider";
 import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 
 const inter = Inter({
   subsets: ["latin"],
 });
 
+const lilex = localFont({
+  src: "../../public/fonts/Lilex-Variable.ttf",
+  display: "swap",
+  variable: "--font-lilex",
+});
+
+const lora = localFont({
+  src: "../../public/fonts/Lora-Variable.ttf",
+  display: "swap",
+  variable: "--font-lora",
+});
+
 export default function Layout({ children }: LayoutProps<"/">) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html
+      lang="en"
+      className={`${inter.className} ${lilex.variable} ${lora.variable} antialiased`}
+      suppressHydrationWarning
+    >
       <body className="flex flex-col min-h-screen font-lilex">
         <RootProvider>{children}</RootProvider>
       </body>
